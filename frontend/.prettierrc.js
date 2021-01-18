2ebdf2a4c6343de1103a8d15d3eb55686d63ef2f
Alexandr <aleksandr.tarabanov@tech-stack.io>
2021-01-18 19:06:10 +0200

    firsComm

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    semi: false,
+    singleQuote: true,
+    arrowParens: 'avoid',
+    useTabs: false, 
+    printWidth: 80, 
+    tabWidth: 4,
+    singleQuote: true, 
+    trailingComma: 'es5', 
+    jsxBracketSameLine: false, 
+
+    semi: false, 
+}
