06c467a52fabc25a39a15b5177c6f7ee002d6f5d
Alexandr <aleksandr.tarabanov@tech-stack.io>
2021-01-21 19:23:49 +0200

    smallCom

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import config from '../ormconfig'
 
 @Module({
     imports: [TypeOrmModule.forRoot(config)],
-    controllers: [AppController],
-    providers: [AppService],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
---
+++ backend/src/entities/users.entity.ts
@@ -0,0 +1,36 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    ManyToOne,
+    Unique,
+} from 'typeorm'
+
+import { IsEmail } from 'class-validator'
+import RoleEntity from './roles.entity'
+
+@Entity('users')
+@Unique(['email'])
+export default class UserEntity {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    @IsEmail()
+    email: string
+
+    @Column()
+    password: string
+
+    @Column({ default: '', length: 100 })
+    firstName: string
+
+    @Column({ default: '', length: 100 })
+    lastName: string
+
+    @Column({ default: '' })
+    image: string
+
+    @ManyToOne(() => RoleEntity, role => role.users)
+    role: RoleEntity
+}
