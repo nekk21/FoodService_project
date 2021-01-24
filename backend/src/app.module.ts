92a051f0601502e810d1841c5f710360b45008b6
nekk <kunkaad@gmail.com>
2021-01-24 13:56:27 +0200

    modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { UsersModule } from './modules/users/users.module'
+import { DishesModule } from './modules/dishes/dishes.module'
+import { OrdersModule } from './modules/orders/orders.module'
 import config from '../ormconfig'
 
 @Module({
-    imports: [TypeOrmModule.forRoot(config)],
+    imports: [
+        TypeOrmModule.forRoot(config),
+        UsersModule,
+        DishesModule,
+        OrdersModule,
+    ],
     controllers: [],
     providers: [],
 })
